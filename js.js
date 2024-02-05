64a41d7eb550d27ac04beb20f13750857e0986e0
Marc-Julius Nadeau <141160865+Markeyjuju@users.noreply.github.com>
2024-02-05 16:46:42 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,24 @@
+const why = [
+    '??',
+    'tes sur?',
+    'vraiment?',
+    'reconsidere',
+    'reponse finale?',
+    "penses s'y",
+]
+const noButton = document.querySelector('button:last-of-type')
+const yesButton = document.querySelector('button:first-of-type')
+
+let scale = 1;
+const no = () => {
+    noButton.innerHTML = why[Math.floor(Math.random()*6)]
+    scale +=0.5
+    yesButton.style.transform = `scale(${scale})`
+}
+
+const careless = () => {
+    const audio = new Audio('careless.mp3')
+    audio.play()
+    document.querySelector('.buttons').remove()
+    document.querySelector('.reveal').style.display = 'flex'
+}
